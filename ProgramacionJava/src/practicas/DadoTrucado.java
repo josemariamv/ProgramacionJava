5fe0ce999a266f8b126426584c3730d509836d01
josemariamv <josemaria@morales-vazquez.com>
2025-11-07 09:08:17 +0100

    Merge branch 'master' of https://github.com/josemariamv/ProgramacionJava

---
+++ ProgramacionJava/src/practicas/DadoTrucado.java
@@ -0,0 +1,40 @@
+package practicas;
+
+import java.util.Scanner;
+
+public class DadoTrucado {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Dime cuantos dados vas a tirar: ");
+		int dados = teclado.nextInt();
+		teclado.close();
+
+		boolean todosIguales = false;
+		int tiradas = 0;
+		int resultados[] = new int[dados];
+		int frecuencias[] = new int[7];
+
+		do {
+			tiradas++;
+			for (int i = 0; i < dados; i++) {
+				int dado = (int) (Math.random() * 6) + 1;
+				frecuencias[dado]++;
+				resultados[i] = dado;
+				if (i == dados - 1)
+					System.out.println(dado);
+				else
+					System.out.print(dado + " - ");
+			}
+			todosIguales = true;
+			for (int j = 1; j < dados; j++) {
+				if (resultados[0] != resultados[j])
+					todosIguales = false;
+			}
+		} while (todosIguales == false);
+		System.out.println("Has necesitado " + tiradas + " tiradas para que salgan todos iguales");
+		for (int k = 1; k <= 6; k++)
+			System.out.printf("El número %d ha salido el %.2f %% de las veces\n", k,
+					(double) (100 * frecuencias[k]) / (dados * tiradas));
+	}
+
+}

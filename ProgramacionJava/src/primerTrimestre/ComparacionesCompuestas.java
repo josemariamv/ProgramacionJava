3f9601fdb874b62be759e74a277ecde562ba8a56
josemariamv <josemaria@morales-vazquez.com>
2025-11-18 09:08:10 +0100

    comparaciones compuestas

---
+++ ProgramacionJava/src/primerTrimestre/ComparacionesCompuestas.java
@@ -0,0 +1,39 @@
+package primerTrimestre;
+
+public class ComparacionesCompuestas {
+
+	public static void main(String[] args) {
+		int n = 5;
+		boolean encontrado = true;
+
+		if (n == 5)
+			System.out.println("n es igual a 5");
+
+		if (n != 6)
+			System.out.println("n no es igual a 6");
+
+		if (encontrado)
+			System.out.println("Lo he encontrado");
+		
+		if(!encontrado)
+			System.out.println("Lo he encontrado");
+		else
+			System.out.println("No lo he encontrado");
+		
+		if (n > 1 && n < 10)
+			System.out.println("n está entre 1 y 10");
+
+		if (n < 2 || n == 5)
+			System.out.println("n es menor a 2 o igual a 5");
+
+		if(encontrado == true || n==5)
+			System.out.println("Lo he encontrado o n es igual a 5");
+
+		if(encontrado == true && n==5)
+			System.out.println("Lo he encontrado y n es igual a 5");
+		
+		if ((n < 2 || n == 5) && encontrado)
+			System.out.println("n es menor a 2 o igual a 5 y además lo he encontrado");
+	}
+
+}

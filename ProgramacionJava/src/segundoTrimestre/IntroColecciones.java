98c2d5670abe84b991f197607462bf2d6dbd6257
josemariamv <jmoralesvazquez@educa.madrid.org>
2025-11-18 20:44:15 +0100

    eliminado

--- ProgramacionJava/src/segundoTrimestre/IntroColecciones.java
+++
@@ -1,9 +0,0 @@
-package segundoTrimestre;
-
-public class IntroColecciones {
-	/*
-	 * 	Las colecciones de Java más útiles y utilizadas se encuentran dentro del Java Collections Framework (JCF).
-	 *  Se clasifican principalmente por las interfaces que implementan, siendo las más comunes: List, Set, y Map
-	 */
-
-}

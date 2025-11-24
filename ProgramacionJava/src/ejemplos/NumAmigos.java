e43333af5c976ad4dee9f1517b75f10e3803b1f2
josemariamv <josemaria@morales-vazquez.com>
2025-11-24 14:01:56 +0100

    numeros amigos y perfectos. reubicación de El Ahorcado

---
+++ ProgramacionJava/src/ejemplos/NumAmigos.java
@@ -0,0 +1,38 @@
+package ejemplos;
+
+public class NumAmigos {
+	public static void main(String[] args) {
+		if (esPerfecto(8128) == true)
+			System.out.println("Es perfecto");
+		else
+			System.out.println("No es perfecto");
+	}
+
+	public static boolean esPerfecto(int n1) {
+		boolean perfecto = false;
+		int suma1 = devuelveSumaDivPropios(n1);
+		if (suma1 == n1)
+			perfecto = true;
+		return perfecto;
+	}
+
+	public static boolean sonAmigos(int n1, int n2) {
+		boolean amigos = false;
+		int suma1 = devuelveSumaDivPropios(n1);
+		int suma2 = devuelveSumaDivPropios(n2);
+		if (suma1 == n2 && suma2 == n1)
+			amigos = true;
+		return amigos;
+	}
+
+	public static int devuelveSumaDivPropios(int num) {
+		int suma = 1;
+		int raiz = (int) Math.sqrt(num) + 1;
+		for (int i = 2; i < raiz; i++)
+			if (num % i == 0) {
+				suma += i;
+				suma += (num / i);
+			}
+		return suma;
+	}
+}
--- ProgramacionJava/src/practicas/elAhorcado.java
+++ ProgramacionJava/src/practicas/elAhorcado.java
@@ -1,4 +1,4 @@
-package ejemplos;
+package practicas;
 
 import java.util.Scanner;
 

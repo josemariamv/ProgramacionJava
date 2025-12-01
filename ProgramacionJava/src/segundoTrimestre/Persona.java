d4b47f731fa64f469c526b49dda4012ce46ba210
josemariamv <jmoralesvazquez@educa.madrid.org>
2025-12-01 09:00:44 +0100

    POO1

--- ProgramacionJava/src/segundoTrimestre/Persona.java
+++ ProgramacionJava/src/segundoTrimestre/Persona.java
@@ -1,17 +1,91 @@
 package segundoTrimestre;
 
+// Crearemos nuestras clases en archivos independientes dentro del mismo paquete donde estará nuestra función main que será el punto 
+// de entrada a nuestro progama, como siempre. Un fichero con una clase como esta pero sin main no puede ejecutarse
+
 public class Persona {
+	/*
+	 * Los atributos de la clase son los datos que luego van a identificar a cada uno de los objetos
+	 * Por defecto los definiremos a todos como privados. Eso hace que nuestra única forma de interactuar 
+	 * con ellos sea a través de los métodos y nos dará protección para que no haya manipulaciones indeseadas
+	 */
 	private String nombre;
 	private String apellidos;
-	private int edad = -1;
+	// se pueden inicializar atributos direcamente en su declaración
+	private int edad = 0;
 	
+	// Un atributo static no es independiente para cada objeto sino que es común a todos los objetos de la clase
+	private static int numPersonas=0;
+	
+	// El método que se llama como la clase es el constructor
+	// Una clase puede tener mas de un constructor. 
 	public Persona(String nom, String ape) {
+		// la palabra reservada this es una referencia al objeto que invoca al método. 
+		// Ya que los atributos son, normalmente, diferentes e independientes para cada objeto por separado
 		this.nombre = nom;
 		this.apellidos = ape;
+		// Los atributos static no usan this
+		numPersonas++;
 	}
 	
+	public Persona(String nomb, String ape, int ed) {
+		this.nombre = nomb;
+		this.apellidos = ape;
+		this.edad = ed;
+		numPersonas++;
+	}
+	
+	/*
+	 * Los métodos de la clase son los que definen su funcionalidad y su comportamiento
+	 * Por lo demás son funciones iguales a las que ya hemos visto. Por defecto todos los métodos
+	 * serán publicos y serán nuestra forma de interactuar con los objetos
+	 * Podemos tener métodos para mostrar datos, para cambiarlos, para recuperarlos o para hacer cualquier cosa
+	 */
 	public void mostrar() {
 		System.out.println(apellidos + ", " + nombre);
 	}
 
+	public void aniversario() {
+		this.edad++;
+	}
+
+	public int cuantasPersonas() {
+		return numPersonas;
+	}
+	
+	// Java no tiene destructores como otros lenguajes de programación
+	// si necesitamos ejecutar algo antes de destruir un objeto creamos un método y
+	// debemos de invocarlo nosotros mismos
+	
+	public void eliminaPersona() {
+		numPersonas--;
+	}
+
+	// Podemos crear automáticamente funciones para interactuar con nuestros atributos
+	// Se llaman getters y setters. Pulsamos sobre el archivo de la clase con el botón derecho
+	// elegimos Source y luego genertar getters y setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 }
+
--- ProgramacionJava/src/segundoTrimestre/Poo1.java
+++ ProgramacionJava/src/segundoTrimestre/Poo1.java
@@ -1,9 +1,44 @@
 package segundoTrimestre;
 
+
 public class Poo1 {
 
 	public static void main(String[] args) {
-		Persona p1 = new Persona("José María", "Morales Vázquez");
+		// Creamos una variable de tipo Persona pero no lo inicializamos. No llamamos a su constructor
+		// ni se reserva memoria para el. Cualquier llamada a un método de esa clase nos dará error
+		// Porque el objeto aún no está creado. No existe aún
+		Persona p1;
+		// Para crear un objeto usamos el operador new y llamamos al constructor de la clase
+		p1 = new Persona("José María", "Morales Vázquez");
+		// Ahora ya podemos llamar a los métodos de la clase
 		p1.mostrar();
+
+		// También podemos hacerlo de una sola vez
+		Persona p2 = new Persona("Isabel", "Gutiérrez Pérez");
+		p2.mostrar();
+		
+		// Si tratamos de mostrar un objeto en consola veremos una referencia pero nada que nos sea útil 
+		System.out.println(p1);
+		// Si  tratamos de mostrar un atributo privado de un objeto no se nos dejará. ¡Es privado!
+		// si lo defines como público si puedes hacerlo. Prueba a ver
+		// System.out.println(p1.nombre);		
+		
+		// Si creamos un nuevo objeto usando una referencia existente o lo igualamos a null
+		// java entiende que el anterior ya no nos sirve y liberará la memoria asignada al 
+		// mismo de forma automática mediante su recolector de basura
+		// si queremos realizar alguna acción antes de su destrucción creamos un método y lo invocamos nosotros
+		// En Java no hay destructores
+		p2.eliminaPersona();
+		p2 = null;
+		p1.eliminaPersona();
+		p1 = new Persona("Andrés", "Pérez, Pérez");
+		
+		// Cuando una clase tiene mas de un constructor distingue a cual llamamos por los argumentos (número y tipo)
+		// Por tanto podemos tener diferentes constructores pero siempre que haya una diferenciación en los argumentos de los mismos
+		Persona p3 = new Persona("José María", "Morales Vázquez", 57);
+		p3.aniversario();
+		
+		System.out.println(p3.cuantasPersonas());
+		System.out.println(p1.cuantasPersonas());
 	}
 }

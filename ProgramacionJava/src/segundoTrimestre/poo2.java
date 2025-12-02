52fa1d372ae4eb2e9bd38020f85ad526897ab377
josemariamv <josemaria@morales-vazquez.com>
2025-12-02 11:05:40 +0100

    cambio chorra

--- ProgramacionJava/src/segundoTrimestre/poo2.java
+++ ProgramacionJava/src/segundoTrimestre/poo2.java
@@ -8,7 +8,10 @@ public class poo2 {
 		Pokemon p2 = new Pokemon("Venasaur");
 		Pokemon p3 = new Pokemon ("Ivysaur");
 		
+		
+		//hola
 		p1.setEvolucion(p2);
+		
 		p2.setEvolucion(p3);
 		
 		p1.mostrar();

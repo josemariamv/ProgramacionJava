878e79291bfba9bb9b856af00d9c6517397dab26
josemariamv <josemaria@morales-vazquez.com>
2025-12-18 11:54:51 +0100

    Merge branch 'master' of https://github.com/josemariamv/ProgramacionJava

---
+++ ProgramacionJava/src/pooProtectora/Animal.java
@@ -0,0 +1,13 @@
+package pooProtectora;
+
+public class Animal {
+
+}
+
+/*
+ * Clientes: nombre, apellidos, edad, tlf, animal en que está interesado
+ * y animales adpotados (3 gatos, 2 perros y 1 tortuga máximo
+ * Animales: año de nacimiento, nombre (opcional), adoptado, vacunado (en perros y gatos) terrestre o no (en tortugas)
+ * Listado de animales adoptados por un cliente
+ * Listado de animales sin adoptar por la protectora
+ */

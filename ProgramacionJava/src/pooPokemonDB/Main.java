d8983bccf0fae03e18cd810245399b53c8529db2
josemariamv <jmoralesvazquez@educa.madrid.org>
2026-05-10 12:00:37 +0200

    Ejemplo de Pokemon DDBB terminado

--- ProgramacionJava/src/pooPokemonDB/Main.java
+++ ProgramacionJava/src/pooPokemonDB/Main.java
@@ -1,10 +1,51 @@
 package pooPokemonDB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<Pokemon> lista = leerDDBB();
+		Collections.sort(lista);
+		for(Pokemon p: lista)
+			System.out.println(p);
 	}
-
+	
+	public static ArrayList<Pokemon> leerDDBB(){
+		ArrayList<Pokemon> lista = new ArrayList<>();
+		String url = "jdbc:mysql://localhost:3306/pokemondb";
+		String usr = "josemaria";
+		String pwd = "abc123";
+		try (Connection conexion = DriverManager.getConnection(url, usr, pwd)) {
+			System.out.println("Conexión realizada con exito\n");
+			PreparedStatement sql = conexion.prepareStatement("SELECT * FROM pokemon");
+			ResultSet r = sql.executeQuery();
+			while (r.next()) {
+				sql = conexion.prepareStatement("SELECT nombre FROM tipo JOIN pokemon_tipo WHERE tipo.id_tipo = pokemon_tipo.id_tipo AND numero_pokedex = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+				sql.setInt(1, r.getInt("numero_pokedex"));
+				ResultSet rTipo = sql.executeQuery();
+				rTipo.last();
+				int numTipos = rTipo.getRow();
+				rTipo.first();
+				if( numTipos == 1)
+					lista.add(new Pokemon(r.getInt("numero_pokedex"), r.getString("nombre"), rTipo.getString("nombre"), r.getDouble("peso"), r.getDouble("altura")));
+				else {
+					String t1 = rTipo.getString("nombre");
+					rTipo.next();
+					String t2 = rTipo.getString("nombre");
+					lista.add(new Pokemon(r.getInt("numero_pokedex"), r.getString("nombre"), t1, t2, r.getDouble("peso"), r.getDouble("altura")));
+				}
+			}
+		} catch (SQLException e) {
+			System.err.println("Error: " + e.getMessage());
+			e.printStackTrace();
+		}	
+		return lista;
+	}
 }
--- ProgramacionJava/src/pooPokemonDB/Pokemon.java
+++ ProgramacionJava/src/pooPokemonDB/Pokemon.java
@@ -1,5 +1,48 @@
 package pooPokemonDB;
 
-public class Pokemon {
+public class Pokemon implements Comparable<Pokemon>{
+	private int codigo;
+	private String nombre;
+	private String[] tipo = new String[2];
+	private double peso;
+	private double altura;
 
+	public Pokemon(int c, String n, String t, double p, double a) {
+		this.codigo = c;
+		this.nombre = n;
+		this.tipo[0] = t;
+		this.peso = p;
+		this.altura = a;
+		this.tipo[1] = null;
+	}
+
+	public Pokemon(int c, String n, String t1, String t2, double p, double a) {
+		// forma elegante de no repetir código entre dos constructores cuando existe una parte común
+		// La llamada al método this que invoca al otro constructor debe de ser siempre la primera línea de este segundo constructor!!!!
+		this(c, n, t1, p, a);
+		this.tipo[1] = t2;
+	}
+	
+	@Override
+	public String toString() {
+		String linea = this.nombre + " (#" + String.valueOf(this.codigo) + ")\n";
+		if(this.tipo[1] == null)
+			linea += "Tipo: " + this.tipo[0] + "\n";
+		else
+			linea += "Tipos: " + this.tipo[0] + " y " + this.tipo[1] + "\n";
+		linea = linea + "Peso: " + String.valueOf(this.peso) + " / Altura: " + String.valueOf(this.altura) + "\n";
+		linea += "---";
+		return linea;
+	}
+	
+	@Override
+	public int compareTo(Pokemon otro) {
+		int resultado = 0;
+		if(this.nombre.compareTo(otro.nombre) > 0)
+			resultado = 1;
+		else if(this.nombre.compareTo(otro.nombre) < 0)
+		 	resultado = -1;
+		return resultado;
+	}
 }
+

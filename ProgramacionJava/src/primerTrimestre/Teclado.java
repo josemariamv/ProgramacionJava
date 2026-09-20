601f55205aa4444aebaaa8dcca378af41d530763
josemariamv <jmoralesvazquez@educa.madrid.org>
2026-09-20 13:16:55 +0200

    teclado

--- ProgramacionJava/src/primerTrimestre/Teclado.java
+++ ProgramacionJava/src/primerTrimestre/Teclado.java
@@ -52,18 +52,60 @@ public class Teclado {
 		// existen otros lectores: nextLong, nextFloat, nextShort, etc. Hay que usar el adecuado siempre
 		// dependiendo de lo que queramos obtener y donde vayamos a guardarlo
 		
-		// cuidado al alternar lectura de enteros y textos:
-		System.out.print("Edad: ");
-		edad = lector.nextInt();
-		System.out.print("Nombre: ");
-		nombre = lector.nextLine();
-
-		System.out.println(nombre + " tiene " + edad);
+		// La clase scanner tiene un pequeño problema cuando leemos números y a continuación frases. Míralo a continuación:
+		
+		lector = new Scanner(System.in);
+				
+		System.out.print("Escribe un número entero: ");
+		int numero = lector.nextInt();
+				
+		System.out.println("Escribe ahora una frase: ");
+		String texto = lector.nextLine();
+		
+		System.out.println(numero);
+		System.out.println(texto);
+				
+		// Si ejecutas lo anterior verás que no te llega a pedir la frase. 
+		// Aparece el mensaje en pantalla pero luego el programa termina
+		// Eso es debido a que después de leer el número (ya sea entero o double) la tecla intro que pulsas
+		// a continuación se queda en el buffer del teclado y es como si escribieras 
+		//una frase sin nada
+				
+		// Para solucionarlo podemos hacer dos cosas
+		// La primera, leer un texto inmediatamente después del número para "limpiar" el buffer:
+				
+		System.out.print("Escribe un número entero: ");
+		numero = lector.nextInt();
+		lector.nextLine(); // Esta lectura limpiará el buffer del teclado y lo dejará listo para leer el texto
+		System.out.print("Escribe ahora una frase: ");
+		texto = lector.nextLine();
+				
+		// Otra estrategia consiste en usar dos objetos diferentes: uno para leer los números
+		// Y otro para leer los textos ya que el buffer no es compartido
+		Scanner otroLector =  new Scanner(System.in);
+				
+		System.out.print("Escribe un número entero: ");
+		numero = lector.nextInt();
+				
+		System.out.print("Escribe ahora una frase: ");
+		texto = otroLector.nextLine();
+				
+		// Fíjate que el problema no ocurre cuando lees dos números seguidos:
+		System.out.print("Escribe un número entero: ");
+		numero = lector.nextInt();
+		System.out.print("Escribe otro mas: ");
+		numero = lector.nextInt();
+				
+		// Ni cuando lees dos textos seguidos:
+		System.out.print("Escribe ahora una frase: ");
+		texto = otroLector.nextLine();
+		System.out.print("Escribe ahora otra frase: ");
+		texto = otroLector.nextLine();
 		
 		
-
 		// No olvides cerrar el scanner cuando termines de usarlo
 		lector.close();
+		otroLector.close();
 	}
 
 }
